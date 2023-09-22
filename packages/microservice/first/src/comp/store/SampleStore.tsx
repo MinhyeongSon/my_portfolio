142837cb2c8fad0f98ade00d5eb08b94cc6138ec
Doping7 <mh271704@gmail.com>
2023-09-23 00:26:12 +0900

    create mono Repository ms part

---
+++ packages/microservice/first/src/comp/store/SampleStore.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SampleStore = () => {
+    return (
+        <></>
+    )
+}
+export default SampleStore;
---
+++ packages/microservice/first/src/comp/store/index.ts
@@ -0,0 +1,1 @@
+export {default as SampleStore} from './SampleStore'
